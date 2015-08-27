10a06c9b1111a647aeb0a5b68cff278c21ab162b
Antonio Garcia <agl0809@gmail.com>
2015-08-27 22:37:26 +0200

    Refactored apply closure pattern

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var five = require("johnny-five");
 var board = new five.Board();
 
-
-// The board's pins will not be accessible until
-// the board has reported that it is ready
 board.on("ready", function () {
-    five.PairBarriers(2,3);
+    var pairBarriers = five.PairBarriers;
+    pairBarriers.init( 2, 3, five );
 });
 
 
