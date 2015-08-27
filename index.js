e90fcd536c3e1e00b1c373e845b391fedd214828
Antonio Garcia <agl0809@gmail.com>
2015-08-27 19:46:26 +0200

    create pairBarriers controller

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ var board = new five.Board();
 // The board's pins will not be accessible until
 // the board has reported that it is ready
 board.on("ready", function () {
-  console.log("Ready!");
-
-  var led = new five.Led(13);
-
-  led.blink(500);
+    five.PairBarriers(2,0);
 });
+
+
