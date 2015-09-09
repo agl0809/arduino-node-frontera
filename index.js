3c815f05abc868f4df38946a7e944f4225055dae
Antonio Garcia <agl0809@gmail.com>
2015-09-10 01:34:07 +0200

    Added mode controller on changedCombination event

--- index.js
+++ index.js
@@ -3,13 +3,33 @@ var board = new five.Board();
 
 board.on("ready", function () {
     var pairBarriers,
-        latestCombination,
         pairBarriers = five.PairBarriers( 2, 3 );
 
-        pairBarriers.on('changedCombination', function(combination){
-            latestCombination = combination;
-            console.log(latestCombination);
-        });
+    var mode = 'FREE';
+    var currentCombination = {
+        external: false,
+        internal: false
+    };
+    var freeCombination = {
+        external: false,
+        internal: false
+    };
+
+    console.log(mode);
+
+    pairBarriers.on('changedCombination', function(combination){
+        currentCombination = combination;
+
+        if( mode === 'FREE') {
+            if (JSON.stringify(currentCombination) != JSON.stringify(freeCombination)) {
+                mode = 'BUSY';
+                console.log(mode);
+            }
+        }else{
+            console.log('I\'m busy...');
+        }
+
+    });
 });
 
 
