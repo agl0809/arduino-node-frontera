17dc16f5c54d11b9a2467177d98f969093a13715
Antonio Garcia <agl0809@gmail.com>
2015-09-10 00:41:14 +0200

    Changed pairbarriers pattern to support events emit

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function () {
-    var pairBarriers = five.PairBarriers;
-    pairBarriers.init( 2, 3, five );
+    var pairBarriers,
+        latestCombination,
+        pairBarriers = five.PairBarriers( 2, 3 );
+
+        pairBarriers.on('changedCombination', function(combination){
+            latestCombination = combination;
+            console.log(latestCombination);
+        });
 });
 
 
