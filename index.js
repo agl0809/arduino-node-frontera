48cece1c7ea62111d78174a5409c0d70aacb0666
Antonio Garcia <agl0809@gmail.com>
2015-09-10 08:57:15 +0200

    Added launche application, manage of modes and control of countdown

--- index.js
+++ index.js
@@ -2,34 +2,101 @@ var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function () {
-    var pairBarriers,
-        pairBarriers = five.PairBarriers( 2, 3 );
+    var mode = 'FREE',
+        countdown = 3000,
+        isCountdownStarted = false,
+        isLocked = false,
+        sequenceCombinations = [].
+        currentCombination = {
+            external: false,
+            internal: false
+        },
+        freeCombination = {
+            external: false,
+            internal: false
+        };
 
-    var mode = 'FREE';
-    var currentCombination = {
-        external: false,
-        internal: false
+    var initialize = function() {
+        var pairBarriers = five.PairBarriers( 2, 3 );
+        pairBarriers.on('changedCombination', function(combination){
+            start(combination);
+        });
     };
-    var freeCombination = {
-        external: false,
-        internal: false
-    };
 
-    console.log(mode);
-
-    pairBarriers.on('changedCombination', function(combination){
+    var start = function (combination) {
         currentCombination = combination;
 
         if( mode === 'FREE') {
-            if (JSON.stringify(currentCombination) != JSON.stringify(freeCombination)) {
-                mode = 'BUSY';
-                console.log(mode);
-            }
+            runFreeMode();
         }else{
-            console.log('I\'m busy...');
+            runBusyMode();
+        }
+    };
+
+    var runFreeMode = function () {
+        if ( !isFreeCombination() ) {
+            mode = 'BUSY';
+            console.log(mode);
+            runBusyMode();
+        }
+    };
+
+    var runBusyMode = function () {
+        if ( isAlreadyUnblocked() ){
+            isLocked = false;
+            mode = 'FREE';
+        }else if ( isNewCountdown() ) {
+            initCountdown();
+            runCountdown();
+            console.log('entro init');
+        }else{
+            console.log('entro run');
+            runCountdown();
         }
 
-    });
+    };
+
+    var isFreeCombination = function () {
+        return JSON.stringify(currentCombination) === JSON.stringify(freeCombination);
+    };
+
+    var isAlreadyUnblocked = function () {
+        return isFreeCombination() &&
+            isLocked === true &&
+            isCountdownStarted === false;
+    };
+
+    var isNewCountdown = function () {
+        return isCountdownStarted != true &&
+            isLocked != true;
+    };
+
+    var initCountdown = function () {
+        sequenceCombinations = [];
+
+        setTimeout(function () {
+            isCountdownStarted = false;
+
+            if ( !isFreeCombination() ) {
+                isLocked = true;
+                console.log('locked');
+            } else {
+                isLocked = false;
+                mode = 'FREE';
+                console.log(mode);
+            }
+
+        }, countdown);
+
+        isCountdownStarted = true;
+    };
+
+    var runCountdown = function () {
+        sequenceCombinations.push(JSON.stringify(currentCombination));
+        console.log(sequenceCombinations);
+    };
+
+    initialize();
 });
 
 
