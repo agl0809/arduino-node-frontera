d23010463c8c10d99198b25ad96feff035170b11
Antonio Garcia <agl0809@gmail.com>
2015-09-11 04:33:23 +0200

    Added summary logic

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var board = new five.Board();
 
 board.on("ready", function () {
     var mode = 'FREE',
-        countdownInterval = 3000,
-        isCountdownStarted = false,
+        countdown,
+        countdownInterval = 6000,
         isLocked = false,
         sequenceCombinations = [],
         sequenceEnter = [
@@ -26,6 +26,15 @@ board.on("ready", function () {
         currentCombination = {
             external: false,
             internal: false
+        },
+        summary = {
+            enter: 0,
+            leave: 0,
+            back: 0,
+            error: 0,
+            inside: function() {
+                return (this.enter - this.leave)
+            }
         };
 
     var initialize = function() {
@@ -33,6 +42,8 @@ board.on("ready", function () {
         pairBarriers.on('changedCombination', function(combination){
             start(combination);
         });
+        renderSummaryInside();
+        renderSummary();
     };
 
     var start = function (combination) {
@@ -48,18 +59,14 @@ board.on("ready", function () {
     var runFreeMode = function () {
         if ( !isFreeCombination() ) {
             mode = 'BUSY';
-            console.log(mode);
             runBusyMode();
         }
     };
 
     var runBusyMode = function () {
-        if ( isAlreadyUnblocked() ){
-            isCountdownStarted = false;
+        if ( isReadyToUnlock() ){
             isLocked = false;
-            mode = 'FREE';
-            console.log( 'unlocked' );
-            console.log( 'Forced FREE' );
+            runCountdown();
         }else if ( isNewCountdown() ) {
             initCountdown();
             runCountdown();
@@ -72,72 +79,136 @@ board.on("ready", function () {
         return JSON.stringify(currentCombination) === JSON.stringify(freeCombination);
     };
 
-    var isAlreadyUnblocked = function () {
+    var isReadyToUnlock = function () {
         return isFreeCombination() &&
             isLocked === true;
     };
 
     var isNewCountdown = function () {
-        return isCountdownStarted != true &&
-            isLocked != true;
+        return  countdown === undefined ||
+                (countdown.ontimeout === null && isLocked != true);
     };
 
     var initCountdown = function () {
         sequenceCombinations = [];
-        setTimeout(countdownCallback, countdownInterval);
-        isCountdownStarted = true;
+        countdown = setTimeout(countdownCallback, countdownInterval);
     };
 
     var countdownCallback = function () {
-        if(mode === 'BUSY') {
-            if (!isFreeCombination()) {
-                isLocked = true;
-                console.log('locked');
-            } else {
-                isCountdownStarted = false;
-                isLocked = false;
-                mode = 'FREE';
-                console.log('Forced FREE');
-            }
+        if( mode === 'BUSY' && !isFreeCombination() ) {
+            isLocked = true;
         }
     };
 
     var runCountdown = function () {
         sequenceCombinations.push(JSON.stringify(currentCombination));
-        console.log( sequenceCombinations );
-        checkSequenceCombinations();
+        matchSequence();
     };
 
-    var checkSequenceCombinations = function () {
-        if( hasSameLengthAsSequenceEnter() &&
-            isSequenceValidCombination( sequenceEnter ) ){
-                console.log('enter');
-                isCountdownStarted = false;
-                mode = 'FREE';
-                console.log( 'Forced FREE' );
-        }
-
-        if( hasSameLengthAsSequenceLeave() &&
-            isSequenceValidCombination( sequenceLeave ) ){
-                console.log('leave');
-                isCountdownStarted = false;
-                mode = 'FREE';
-                console.log( 'Forced FREE' );
-        }
+    var destroyCountdown = function () {
+        clearTimeout(countdown);
     };
 
-    var hasSameLengthAsSequenceEnter = function () {
+    var closeCountdown = function () {
+        destroyCountdown();
+        isLocked = false;
+        mode = 'FREE';
+        renderSummaryInside();
+        renderSummary();
+    };
+
+    var hasSameLengthAsEnter = function () {
         return sequenceCombinations.length === sequenceEnter.length;
     };
 
-    var hasSameLengthAsSequenceLeave = function () {
+    var hasSameLengthAsLeave = function () {
         return sequenceCombinations.length === sequenceLeave.length;
     };
 
-    var isSequenceValidCombination = function ( sequenceValid ){
+    var matchSequence = function () {
+        if( hasSameLengthAsEnter() &&
+            isValidSequence( sequenceEnter ) ){
+            enterProcess();
+            closeCountdown();
+        }
+
+        if( hasSameLengthAsLeave() &&
+            isValidSequence( sequenceLeave ) ){
+            leaveProcess();
+            closeCountdown();
+        }
+
+        if( sequenceCombinations.length > sequenceEnter.length  &&
+            sequenceCombinations[0] === sequenceEnter[0] &&
+            sequenceCombinations[sequenceCombinations.length-2] === sequenceEnter[sequenceEnter.length-2] &&
+            isFreeCombination() ){
+            enterProcess();
+            closeCountdown();
+        }
+
+        if( sequenceCombinations.length > sequenceLeave.length  &&
+            sequenceCombinations[0] === sequenceLeave[0] &&
+            sequenceCombinations[sequenceCombinations.length-2] === sequenceLeave[sequenceLeave.length-2] &&
+            isFreeCombination() ){
+            leaveProcess();
+            closeCountdown();
+        }
+
+        if( sequenceCombinations.length > sequenceEnter.length  &&
+            sequenceCombinations[0] === sequenceEnter[0] &&
+            sequenceCombinations[sequenceCombinations.length-2] != sequenceEnter[sequenceEnter.length-2] &&
+            isFreeCombination()){
+            backProcess();
+            closeCountdown();
+        }
+
+        if( sequenceCombinations.length > sequenceLeave.length  &&
+            sequenceCombinations[0] === sequenceLeave[0] &&
+            sequenceCombinations[sequenceCombinations.length-2] != sequenceLeave[sequenceLeave.length-2] &&
+            isFreeCombination()){
+            backProcess();
+            closeCountdown();
+        }
+
+        if( sequenceCombinations.length < sequenceLeave.length  &&
+            isFreeCombination() ){
+            backProcess();
+            closeCountdown();
+        }
+    };
+
+    var isValidSequence = function ( sequenceValid ) {
         return sequenceCombinations.every(function(element, index) {
             return element === sequenceValid[index]
         });
+    };
+
+    var enterProcess = function () {
+        summary.enter += 1;
+    };
+
+    var leaveProcess = function () {
+        if( ( summary.enter - summary.leave ) > 0 ) {
+            summary.leave += 1;
+        }else{
+            errorProcess();
+        }
+    };
+
+    var backProcess = function () {
+        summary.back += 1;
+    };
+
+    var errorProcess = function () {
+        summary.error += 1;
+    };
+
+    var renderSummary = function () {
+        console.log('Summary ', JSON.stringify(summary));
+    };
+
+    var renderSummaryInside = function () {
+        console.log('Inside ', summary.inside());
     };
 
     initialize();
